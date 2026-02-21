440731b1d7155587c2fb5d81c15cb65c477c3c08
arlein015 <rachehua45@gmail.com>
2026-02-21 07:39:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,39 @@
+const CACHE_NAME = 'luther-fashion-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/badg noir.JPG'
+];
 
+// Installation du Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Stratégie de cache : Réseau d'abord, sinon Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
